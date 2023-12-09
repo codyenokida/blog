191c437c8bd9e79c992e88f063c262427045355e
codyenokida <codyenokida@gmail.com>
2023-12-08 19:07:29 -0800

    Removed dotenv

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,19 +3,17 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
-require("dotenv").config();
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.ENV_FIREBASE_API_KEY,
-  authDomain: process.env.ENV_AUTH_DOMAIN,
-  projectId: process.env.ENV_PROJECT_ID,
-  storageBucket: process.env.ENV_STORAGE_BUCKET,
-  messagingSenderId: process.env.ENV_MESSAGING_SENDER_ID,
-  appId: process.env.ENV_APP_ID,
-  measurementId: process.env.ENV_MEASUREMENT_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 // Initialize Firebase
 
