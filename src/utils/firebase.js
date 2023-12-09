b6a3df8f2dff5b090f5b8fb8f1eacda3fe605699
codyenokida <codyenokida@gmail.com>
2023-12-08 18:58:14 -0800

    Additional env variables

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,17 +3,19 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
+require("dotenv").config();
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyADbkWgT9sf363Mk5hn8jFuKnEhL8YaygQ",
-  authDomain: "kota-blog-efceb.firebaseapp.com",
-  projectId: "kota-blog-efceb",
-  storageBucket: "kota-blog-efceb.appspot.com",
-  messagingSenderId: "511806902822",
-  appId: "1:511806902822:web:96ff45cf59e27d667c8ce7",
-  measurementId: "G-8V58VSF47W"
+  apiKey: process.env.ENV_FIREBASE_API_KEY,
+  authDomain: process.env.ENV_AUTH_DOMAIN,
+  projectId: process.env.ENV_PROJECT_ID,
+  storageBucket: process.env.ENV_STORAGE_BUCKET,
+  messagingSenderId: process.env.ENV_MESSAGING_SENDER_ID,
+  appId: process.env.ENV_APP_ID,
+  measurementId: process.env.ENV_MEASUREMENT_ID,
 };
 // Initialize Firebase
 
@@ -23,4 +25,3 @@ const app = initializeApp(firebaseConfig);
 // It will be imported into your react app whenever it is needed
 export const db = getFirestore(app);
 export const storage = getStorage(app);
-
